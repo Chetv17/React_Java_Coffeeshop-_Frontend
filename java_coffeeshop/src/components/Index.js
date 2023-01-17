922297ece545984bfef7794c8b5f3739d80eec3a
Chester Vincent <chetvincent@Chesters-MacBook-Pro-2.local>
2023-01-17 16:51:47 -0500

    front end functional

---
+++ java_coffeeshop/src/components/Index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import '../App.css';
+
+const Index = () => {
+
+
+    return (
+        <>
+           <main id="Index">
+            <h2>Hello World!</h2>
+           </main>
+        </>
+
+    )
+}
+
+export default Index;
---
+++ java_coffeeshop/src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import '../App.css';
+
+const Nav = () => {
+
+
+    return (
+        <>
+            <h1>Redwood Coffee Roasters</h1>
+        </>
+
+    )
+}
+
+export default Nav;
